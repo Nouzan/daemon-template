68d361c9e537561e3e5e95ebb70321afa6310c15
Nouzan <clobraindie@outlook.com>
2020-06-25 23:18:20 +0800

    cli template with --verbose support and anyhow

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use clap_verbosity_flag::Verbosity;
+use structopt::StructOpt;
+use simplelog::{ Config, SimpleLogger };
+use anyhow::Result;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(flatten)]
+    verbose: Verbosity,
+}
+
+fn main() -> Result<()>{
+    let opt = Opt::from_args();
+    if let Some(level) = opt.verbose.log_level() {
+        SimpleLogger::init(level.to_level_filter(), Config::default())?;
+    }
+    log::debug!("opt={:?}", opt.verbose.log_level());
+    Ok(())
+}
